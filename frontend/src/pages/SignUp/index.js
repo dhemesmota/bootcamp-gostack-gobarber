53f67bd9de81bc191e2e599beb4c1c932b7a9eaa
Dhemes Mota <dhemes.mota@gmail.com>
2019-11-07 22:31:53 -0300

    Configurando signup

--- frontend/src/pages/SignUp/index.js
+++ frontend/src/pages/SignUp/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
+import Lottie from 'react-lottie';
 
 import logo from '~/assets/logo.svg';
+import loader from '~/assets/loader1.json';
+
+import { signUpRequest } from '~/store/modules/auth/actions';
 
 const schema = Yup.object().shape({
   name: Yup.string().required('O nome é obrigatório'),
@@ -16,9 +21,21 @@ const schema = Yup.object().shape({
 });
 
 export default function SignUp() {
-  function handleSubmit(data) {
-    console.tron.log(data);
+  const dispatch = useDispatch();
+  const loading = useSelector(state => state.auth.loading);
+
+  function handleSubmit({ name, email, password }) {
+    dispatch(signUpRequest(name, email, password));
   }
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: loader,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
 
   return (
     <>
@@ -33,7 +50,13 @@ export default function SignUp() {
           placeholder="Sua senha secreta"
         />
 
-        <button type="submit">Criar conta</button>
+        <button type="submit">
+          {loading ? (
+            <Lottie options={defaultOptions} height={20} width={100} />
+          ) : (
+            'Criar conta'
+          )}
+        </button>
         <Link to="/">Já tenho login</Link>
       </Form>
     </>
