eb60b81c327e7205b88de3d5a3da4046553b26b4
Dhemes Mota <dhemes.mota@gmail.com>
2019-11-08 10:27:49 -0300

    Estilizando perfil

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import Notifications from '~/components/Notifications';
@@ -7,6 +8,8 @@ import { Container, Content, Profile } from './styles';
 import logo from '~/assets/logo-purple.svg';
 
 export default function Header() {
+  const { profile } = useSelector(state => state.user);
+
   return (
     <Container>
       <Content>
@@ -20,12 +23,12 @@ export default function Header() {
 
           <Profile>
             <div>
-              <strong>Dhemes Mota</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu perfil</Link>
             </div>
             <img
               src="https://api.adorable.io/avatars/50/abott@adorable.png"
-              alt="Dhemes Mota"
+              alt={profile.name}
             />
           </Profile>
         </aside>
