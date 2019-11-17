2a50f6508745d1b8c354008ed1f43f7068ff5323
Dhemes Mota <dhemes.mota@gmail.com>
2019-11-17 02:54:44 -0300

    Estilizando agendamentos

---
+++ mobile/src/components/Appointment/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, Left, Avatar, Info, Name, Time } from './styles';
+
+export default function Appointment() {
+  return (
+    <Container>
+      <Left>
+        <Avatar
+          source={{ uri: 'https://api.adorable.io/avatar/50/gobarber.png' }}
+        />
+
+        <Info>
+          <Name>Dhemes Mota</Name>
+          <Time>em 3 horas</Time>
+        </Info>
+      </Left>
+
+      <TouchableOpacity onPress={() => {}}>
+        <Icon name="event-busy" color="#f64c75" size={20} />
+      </TouchableOpacity>
+    </Container>
+  );
+}
--- mobile/src/pages/Dashboard/index.js
+++ mobile/src/pages/Dashboard/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
-import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import Background from '~/components/Background';
+import Appointment from '~/components/Appointment';
+
+import { Container, Title, List } from './styles';
 
 export default function Dashboard() {
-  return <View />;
+  const DATA = [1, 2, 3, 4, 5];
+
+  return (
+    <Background>
+      <Container>
+        <Title>Agendamentos</Title>
+
+        <List
+          data={DATA}
+          keyExtractor={item => String(item)}
+          renderItem={({ item }) => <Appointment data={item} />}
+        />
+      </Container>
+    </Background>
+  );
 }
+
+Dashboard.navigationOptions = {
+  tabBarLabel: 'Agendamentos',
+  tabBarIcon: ({ tintColor }) => (
+    <Icon name="event" size={20} color={tintColor} />
+  ),
+};
