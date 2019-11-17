51e07e9bd6b0ab4ebd5eb73e3b6c26aec0d0f84c
Dhemes Mota <dhemes.mota@gmail.com>
2019-11-17 19:19:48 -0300

    Listando prestadores de serviços

--- mobile/src/pages/New/SelectProvider/index.js
+++ mobile/src/pages/New/SelectProvider/index.js
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import api from '~/services/api';
+
 import Background from '~/components/Background';
 
-// import { Container } from './styles';
+import { Container, ProviderList, Provider, Avatar, Name } from './styles';
 
 export default function SelectProvider() {
-  return <Background />;
+  const [providers, setProviders] = useState([]);
+
+  useEffect(() => {
+    async function loadProviders() {
+      const response = await api.get('/providers');
+
+      setProviders(response.data);
+    }
+
+    loadProviders();
+  }, []);
+
+  return (
+    <Background>
+      <Container>
+        <ProviderList
+          data={providers}
+          keyExtractor={provider => String(provider.id)}
+          renderItem={({ item }) => (
+            <Provider>
+              <Avatar
+                source={{
+                  // eslint-disable-next-line no-nested-ternary
+                  uri: item.avatar
+                    ? __DEV__
+                      ? `http://10.0.2.2:3333/files/${item.avatar.path}`
+                      : item.avatar.url
+                    : `https://api.adorable.io/avatar/50/${item.name}.png`,
+                }}
+              />
+              <Name>{item.name}</Name>
+            </Provider>
+          )}
+        />
+      </Container>
+    </Background>
+  );
 }
 
 SelectProvider.navigationOptions = ({ navigation }) => ({
